b0bd2bcfcac06a7eb0112add64823355a56131dc
mistercow <esteban.lagos21@gmail.com>
2025-08-27 00:45:55 -0400

    Controller basico de categorias para obtener y registrar datos de categorias

--- src/main/java/com/lagos/ecommerce/controller/CategoriaController.java
+++ src/main/java/com/lagos/ecommerce/controller/CategoriaController.java
@@ -1,4 +1,28 @@
 package com.lagos.ecommerce.controller;
 
+import com.lagos.ecommerce.domain.categorias.Categoria;
+import com.lagos.ecommerce.domain.categorias.CategoriaRepository;
+import com.lagos.ecommerce.domain.categorias.DatosCategoria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> obtenerCategorias(){
+        return categoriaRepository.findAll();
+    }
+
+    @PostMapping
+    public void crearCategoria(@RequestBody DatosCategoria datosCategoria){
+        Categoria categoria = new Categoria(null, datosCategoria.nombre(), datosCategoria.descripcion(), null);
+        categoriaRepository.save(categoria);
+    }
 }
