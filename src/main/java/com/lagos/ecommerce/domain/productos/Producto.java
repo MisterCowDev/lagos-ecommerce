31378408b8dc9f8ef3cff23ba59be33cc9418dbe
mistercow <esteban.lagos21@gmail.com>
2025-08-27 00:02:06 -0400

    Creada la entidad producto

--- src/main/java/com/lagos/ecommerce/domain/productos/Producto.java
+++ src/main/java/com/lagos/ecommerce/domain/productos/Producto.java
@@ -1,4 +1,33 @@
 package com.lagos.ecommerce.domain.productos;
 
+import com.lagos.ecommerce.domain.categorias.Categoria;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Producto")
+@Table(name = "productos")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private Integer stock;
+    private String imagen;
+
+    @Enumerated(EnumType.STRING)
+    private Estado estado;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
 }
